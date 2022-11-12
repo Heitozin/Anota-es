0f241810dd1ad14b2a821d22d2b2d3afda2e0271
Heitô <98980899+Heitozin@users.noreply.github.com>
2022-11-12 19:38:35 -0300

    Add files via upload

---
+++ Anotações/anotcoes.js
@@ -0,0 +1,27 @@
+/*for(let posicao = 0; posicao < nomeProduto.length; posicao++){
+
+        valor_total = preco[posicao] * quantidade[posicao] 
+        mostrarRel += `<p>Produto: ${nomeProduto[posicao]} | Quantidade de produtos: ${quantidade[posicao]} | Valor total: ${valor_total}`
+    }*/
+    /*for(let posicao = 1; posicao <= 5; posicao++){
+    if(quantidade[posicao] > 1){
+    valor_total = preco_produto[posicao] * quantidade[posicao]
+}
+valor_total = preco[posicao] + valor_total;
+}
+
+totalPagar.innerHTML = "";
+totalPagar.innerHTML = valor_total;
+})
+
+Para contador de 1 ate 4 faca
+
+let soma = 0;
+
+for(let produtos = 1; produtos <= 5 ; produtos++){
+    let preco = prompt ('Digite o valor do produto:');
+
+    soma = Number(preco) + soma;
+}
+
+alert('TOTAL DA COMPRA R$' + soma)*/
